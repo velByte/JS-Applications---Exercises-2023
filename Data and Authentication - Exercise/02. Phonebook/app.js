b8c5ef0ba45c2a2ef7bb1801102164fe1a3af9d6
velByte <67099502+velByte@users.noreply.github.com>
2023-03-05 14:51:43 +0100

    Added solution

--- Data and Authentication - Exercise/02. Phonebook/app.js
+++ Data and Authentication - Exercise/02. Phonebook/app.js
@@ -1,5 +1,85 @@
 function attachEvents() {
-    console.log('TODO...');
+  const url = "http://localhost:3030/jsonstore/phonebook";
+
+  const loadBtn = document.getElementById("btnLoad");
+  const createBtn = document.getElementById("btnCreate");
+
+  loadBtn.addEventListener("click", loadHendler);
+  createBtn.addEventListener("click", createHendler);
+
+  async function createHendler(e) {
+    try {
+      const personeInput = document.getElementById("person").value;
+      const phoneInput = document.getElementById("phone").value;
+      document.getElementById("person").value = "";
+      document.getElementById("phone").value = "";
+
+      //TODO: Add the Fetch
+
+      const data = {
+        person: personeInput,
+        phone: phoneInput,
+      };
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if(!response.ok) throw new Error(); 
+      else console.log("Send successful");
+      loadBtn.dispatchEvent(new Event("click"));
+
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  async function loadHendler(e) {
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      const data = await res.json();
+      const phonebook = document.getElementById("phonebook");
+      phonebook.innerHTML = "";
+
+      Object.entries(data).forEach(([key, value]) => {
+        const person = value.person;
+        const phone = value.phone;
+        const id = value._id;
+
+        const li = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+
+        deleteBtn.addEventListener("click",async () => {
+            try {
+                const response = await fetch(`${url}/${key}`, {
+                    method: "DELETE",
+                    headers: {
+                      "Content-Type": "application/json",
+                    }
+                  });
+                  if(!response.ok) throw new Error(); 
+                  else console.log("Delete successful");
+                 
+            } catch (error) {
+                console.log(error);
+            }
+        });
+
+        deleteBtn.textContent = "Delete";
+        li.textContent = `${person}: ${phone}`;
+        li.id = id;
+        li.appendChild(deleteBtn);
+        phonebook.appendChild(li);
+      });
+    } catch (error) {}
+    
+  }
 }
 
 attachEvents();
--- Data and Authentication - Exercise/03. Students/app.js
+++ Data and Authentication - Exercise/03. Students/app.js
@@ -1,1 +1,76 @@
-console.log('TODO...');
+const form = document.getElementById("form");
+const submitBtn = document.getElementById("submit");
+const url = "http://localhost:3030/jsonstore/collections/students";
+
+loadStudents();
+
+submitBtn.addEventListener("click", (event) => {
+//   event.preventDefault();
+
+  const formData = new FormData(form);
+  const data = {};
+  for (const [key, value] of formData.entries()) {
+    data[key] = value;
+  }
+
+  const allKeysNotEmpty = isValidStudent(data);
+  if (!allKeysNotEmpty) return;
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      if (res.ok) {
+        console.log("Data sent successfully");
+      } else {
+        console.error("Error sending data");
+      }
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+    });
+});
+
+async function loadStudents() {
+  const tbody = document.querySelector("#results tbody");
+  tbody.innerHTML = ""; //Remove all data 
+
+  try {
+    const res = await fetch(url);
+    if (!res.ok) throw new Error();
+    const data = await res.json();
+
+    Object.entries(data).forEach(([key, value]) => {
+      const firstName = value.firstName; 
+      const lastName = value.lastName; 
+      const facultyNumber = value.facultyNumber; 
+      const grade = value.grade; 
+      const id = value._id
+      const newRow = document.createElement("tr");
+      newRow.id = id; 
+      newRow.innerHTML = `<td>${firstName}</td><td>${lastName}</td><td>${facultyNumber}</td><td>${grade}</td>`;
+      tbody.appendChild(newRow);
+    });
+  } catch (error) {}
+}
+
+function isValidStudent(obj) {
+  if (
+    typeof obj.firstName !== "string" ||
+    obj.firstName.trim() === "" ||
+    typeof obj.lastName !== "string" ||
+    obj.lastName.trim() === "" ||
+    !/^\d+$/.test(obj.facultyNumber) ||
+    obj.facultyNumber.trim() === "" ||
+    isNaN(obj.grade) ||
+    obj.grade.trim() === ""
+  ) {
+    return false;
+  }
+
+  return true;
+}
